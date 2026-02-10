aae353a07e244c42cda48be84a18640c32770fa7
Jesse Green <jessegreen0906@gmail.com>
2026-02-10 15:58:44 +1100

    Update project files

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,15 @@
 import { defineBackend } from "@aws-amplify/backend";
-import { configureSubscriptionsDatastore } from "./datastore/resource";
+import { createSubscriptionsDatastoreResources } from "./datastore/resource";
 
 const backend = defineBackend({});
-configureSubscriptionsDatastore(backend);
+const subscriptionsStack = backend.createStack("subscriptions-datastore");
+const subscriptionsTable = createSubscriptionsDatastoreResources(subscriptionsStack);
+
+backend.addOutput({
+  custom: {
+    SUBSCRIPTIONS_TABLE_NAME: subscriptionsTable.tableName,
+    SUBSCRIPTIONS_TABLE_ARN: subscriptionsTable.tableArn,
+  },
+});
 
 export { backend };
